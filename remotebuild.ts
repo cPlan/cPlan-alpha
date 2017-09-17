3cb306b57c55625f6fbb8d7827b467b9f5ef1e85
ThomasHellum <dr.hellum@frigg.solutions>
2017-09-17 13:50:10 +0200

    Updatet build script

--- remotebuild.ts
+++ remotebuild.ts
@@ -4,14 +4,16 @@ import * as yargs from 'yargs';
 const cliArguments: any = yargs.argv;
 
 const options = {
-    data: {
-        pull: true,
-        build_count: null
+    form: {
+        data: {
+            pull: true,
+            build_count: null
+        }
     }
 };
 
 if (cliArguments.b) {
-    options.data.build_count = cliArguments.b as number;
+    options.form.data.build_count = cliArguments.b as number;
 }
 
 client.auth({token: cliArguments.t}, function (e, api) {
@@ -19,8 +21,4 @@ client.auth({token: cliArguments.t}, function (e, api) {
         console.log('error:', e);
         console.log('data:', data);
     });
-    // api.post('/apps/2798685/build', (e, data) => {
-    //     console.log('error:', e);
-    //     console.log('data:', data);
-    // });
 });
